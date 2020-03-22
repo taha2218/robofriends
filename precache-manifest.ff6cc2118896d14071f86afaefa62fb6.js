fabb3883d410cc88fc50f3a669d43cfdff9f6bb8
taha2218 <ta.22.ha.18.s@gmail.com>
2020-03-22 15:49:31 +0530

    Updates

---
+++ precache-manifest.ff6cc2118896d14071f86afaefa62fb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1dcf2e5ab766388f73624d836bc2af2",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "45fbf456fca079d803d6",
+    "url": "/robofriends/static/css/main.d7484884.chunk.css"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/robofriends/static/js/2.43071c07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.43071c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45fbf456fca079d803d6",
+    "url": "/robofriends/static/js/main.19a6aae9.chunk.js"
+  },
+  {
+    "revision": "46690a7b9ddc777b3b4c",
+    "url": "/robofriends/static/js/runtime-main.ba4515c0.js"
+  }
+]);
